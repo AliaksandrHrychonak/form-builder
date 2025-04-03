7929ad486fbed871c12ab946d5c9637f5a9e00d7
AliaksandrHrychonak <@icloud.com>
2025-04-03 12:09:59 +0300

    feat: add helper module

--- backend/src/common/common.module.ts
+++ backend/src/common/common.module.ts
@@ -6,59 +6,58 @@ import { DatabaseOptionsService } from '@common/database/services/database.optio
 import * as Joi from 'joi';
 import { ENUM_APP_ENVIRONMENT } from '@app/app/constants/app.enum.constant';
 import { APP_LANGUAGE } from '@app/app/constants/app.constant';
+import { HelperModule } from '@common/helper/helper.module';
 
 export enum ENUM_MESSAGE_LANGUAGE {
-  EN = 'en',
-  pl = 'pl',
+    EN = 'en',
+    pl = 'pl',
 }
 
 @Module({
-  controllers: [],
-  imports: [
-    ConfigModule.forRoot({
-      load: configs,
-      isGlobal: true,
-      cache: true,
-      envFilePath: ['.env'],
-      expandVariables: true,
-      validationSchema: Joi.object({
-        APP_NAME: Joi.string().required(),
-        APP_ENV: Joi.string()
-          .valid(...Object.values(ENUM_APP_ENVIRONMENT))
-          .default('development')
-          .required(),
-        APP_LANGUAGE: Joi.string()
-          .valid(...Object.values(ENUM_MESSAGE_LANGUAGE))
-          .default(APP_LANGUAGE)
-          .required(),
+    controllers: [],
+    imports: [
+        ConfigModule.forRoot({
+            load: configs,
+            isGlobal: true,
+            cache: true,
+            envFilePath: ['.env'],
+            expandVariables: true,
+            validationSchema: Joi.object({
+                APP_NAME: Joi.string().required(),
+                APP_ENV: Joi.string()
+                    .valid(...Object.values(ENUM_APP_ENVIRONMENT))
+                    .default('development')
+                    .required(),
+                APP_LANGUAGE: Joi.string()
+                    .valid(...Object.values(ENUM_MESSAGE_LANGUAGE))
+                    .default(APP_LANGUAGE)
+                    .required(),
 
-        HTTP_ENABLE: Joi.boolean().default(true).required(),
-        HTTP_HOST: [
-          Joi.string().ip({ version: 'ipv4' }).required(),
-          Joi.valid('localhost').required(),
-        ],
-        HTTP_PORT: Joi.number().default(3000).required(),
-        HTTP_VERSIONING_ENABLE: Joi.boolean().default(true).required(),
-        HTTP_VERSION: Joi.number().required(),
+                HTTP_ENABLE: Joi.boolean().default(true).required(),
+                HTTP_HOST: [Joi.string().ip({ version: 'ipv4' }).required(), Joi.valid('localhost').required()],
+                HTTP_PORT: Joi.number().default(3000).required(),
+                HTTP_VERSIONING_ENABLE: Joi.boolean().default(true).required(),
+                HTTP_VERSION: Joi.number().required(),
 
-        DATABASE_HOST: Joi.string().default('localhost').required(),
-        DATABASE_PORT: Joi.string().default('5432').required(),
-        DATABASE_NAME: Joi.string().default('forms_builder_db').required(),
-        DATABASE_USER: Joi.string().allow(null, '').optional(),
-        DATABASE_PASSWORD: Joi.string().allow(null, '').optional(),
-        DATABASE_DEBUG: Joi.boolean().default(false).required(),
-        DATABASE_OPTIONS: Joi.string().allow(null, '').optional(),
-      }),
-      validationOptions: {
-        allowUnknown: true,
-        abortEarly: true,
-      },
-    }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useClass: DatabaseOptionsService,
-      inject: [ConfigService],
-    }),
-  ],
+                DATABASE_HOST: Joi.string().default('localhost').required(),
+                DATABASE_PORT: Joi.string().default('5432').required(),
+                DATABASE_NAME: Joi.string().default('forms_builder_db').required(),
+                DATABASE_USER: Joi.string().allow(null, '').optional(),
+                DATABASE_PASSWORD: Joi.string().allow(null, '').optional(),
+                DATABASE_DEBUG: Joi.boolean().default(false).required(),
+                DATABASE_OPTIONS: Joi.string().allow(null, '').optional(),
+            }),
+            validationOptions: {
+                allowUnknown: true,
+                abortEarly: true,
+            },
+        }),
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            useClass: DatabaseOptionsService,
+            inject: [ConfigService],
+        }),
+        HelperModule,
+    ],
 })
 export class CommonModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,61 +5,59 @@ import { ConfigService } from '@nestjs/config';
 import { useContainer } from 'class-validator';
 
 async function bootstrap() {
-  const app: NestApplication = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
+    const app: NestApplication = await NestFactory.create(AppModule);
+    const configService = app.get(ConfigService);
 
-  const databaseHost = configService.get('database.host') as string;
-  const databasePort = configService.get('database.port') as string;
-  const env = configService.get('app.env') as string;
-  const host = configService.get('app.http.host') as string;
-  const port = configService.get('app.http.port') as number;
-  const globalPrefix = configService.get('app.globalPrefix') as string;
-  const versioningPrefix = configService.get('app.versioning.prefix') as string;
-  const version = configService.get('app.versioning.version') as string;
+    const databaseHost = configService.get('database.host') as string;
+    const databasePort = configService.get('database.port') as string;
+    const env = configService.get('app.env') as string;
+    const host = configService.get('app.http.host') as string;
+    const port = configService.get('app.http.port') as number;
+    const globalPrefix = configService.get('app.globalPrefix') as string;
+    const versioningPrefix = configService.get('app.versioning.prefix') as string;
+    const version = configService.get('app.versioning.version') as string;
 
-  const httpEnable = configService.get('app.http.enable') as boolean;
-  const versionEnable = configService.get('app.versioning.enable') as string;
+    const httpEnable = configService.get('app.http.enable') as boolean;
+    const versionEnable = configService.get('app.versioning.enable') as string;
 
-  const logger = new Logger();
-  process.env.NODE_ENV = env;
+    const logger = new Logger();
+    process.env.NODE_ENV = env;
 
-  // Global
-  app.setGlobalPrefix(globalPrefix);
+    // Global
+    app.setGlobalPrefix(globalPrefix);
 
-  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+    useContainer(app.select(AppModule), { fallbackOnErrors: true });
 
-  // Versioning
-  if (versionEnable) {
-    app.enableVersioning({
-      type: VersioningType.URI,
-      defaultVersion: version,
-      prefix: versioningPrefix,
-    });
-  }
+    // Versioning
+    if (versionEnable) {
+        app.enableVersioning({
+            type: VersioningType.URI,
+            defaultVersion: version,
+            prefix: versioningPrefix,
+        });
+    }
 
-  // Listen
-  await app.listen(port, host);
+    // Listen
+    await app.listen(port, host);
 
-  logger.log(`==========================================================`);
+    logger.log(`==========================================================`);
 
-  logger.log(`Environment Variable`, 'NestApplication');
-  logger.log(JSON.parse(JSON.stringify(process.env)), 'NestApplication');
+    logger.log(`Environment Variable`, 'NestApplication');
+    logger.log(JSON.parse(JSON.stringify(process.env)), 'NestApplication');
 
-  logger.log(`==========================================================`);
+    logger.log(`==========================================================`);
 
-  logger.log(
-    `Http is ${httpEnable}, ${
-      httpEnable ? 'routes registered' : 'no routes registered'
-    }`,
-    'NestApplication',
-  );
-  logger.log(`Http versioning is ${versionEnable}`, 'NestApplication');
+    logger.log(
+        `Http is ${httpEnable}, ${httpEnable ? 'routes registered' : 'no routes registered'}`,
+        'NestApplication'
+    );
+    logger.log(`Http versioning is ${versionEnable}`, 'NestApplication');
 
-  logger.log(`Http Server running on ${await app.getUrl()}`, 'NestApplication');
-  logger.log(`Database host ${databaseHost}`, 'NestApplication');
-  logger.log(`Database port ${databasePort}`, 'NestApplication');
+    logger.log(`Http Server running on ${await app.getUrl()}`, 'NestApplication');
+    logger.log(`Database host ${databaseHost}`, 'NestApplication');
+    logger.log(`Database port ${databasePort}`, 'NestApplication');
 
-  logger.log(`==========================================================`);
+    logger.log(`==========================================================`);
 }
 
 bootstrap();
