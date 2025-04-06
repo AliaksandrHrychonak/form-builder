ea3465acb6ecf41dc3dab04f4405b3121328ef81
AliaksandrHrychonak <@icloud.com>
2025-04-07 01:47:35 +0300

    feat(config): add modular configuration system
    Introduce a centralized and modular configuration system using `@nestjs/config`. This includes configurations for app, auth, AWS, database, middleware, requests, and more. Each module is designed to simplify environment-based settings management and improve project maintainability.

---
+++ backend/src/configs/database.config.ts
@@ -0,0 +1,20 @@
+import { registerAs } from '@nestjs/config';
+
+export default registerAs(
+    'database',
+    (): Record<string, any> => ({
+        // uri:
+        //     process.env?.DATABASE_URI ??
+        //     'mongodb://localhost:27017,localhost:27018,localhost:27019',
+        uri:
+            process.env?.DATABASE_URI ??
+            'mongodb://localhost:27017',
+
+        debug: process.env.DATABASE_DEBUG === 'true',
+        timeoutOptions: {
+            serverSelectionTimeoutMS: 10000,
+            socketTimeoutMS: 10000,
+            heartbeatFrequencyMS: 30000,
+        },
+    })
+);
