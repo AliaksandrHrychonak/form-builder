2179d98d41bc3e81950ece1742f5c4e4ac803333
AliaksandrHrychonak <@icloud.com>
2025-04-07 01:49:37 +0300

    feat: add app core setup with middlewares and filters
    Introduced essential middlewares (e.g., CORS, Helmet, body-parser) and request filters for error handling (e.g., validation, HTTP exceptions). Also added core app enum constants, DTOs, interfaces, and module registration for improved structure and configuration.

---
+++ backend/src/app/constants/app.enum.constant.ts
@@ -0,0 +1,10 @@
+export enum ENUM_APP_ENVIRONMENT {
+    PRODUCTION = 'production',
+    STAGING = 'staging',
+    DEVELOPMENT = 'development',
+}
+
+export enum ENUM_APP_TIMEZONE {
+    EUROPE_MINSK = 'Europe/Minsk',
+    EUROPE_WARSAW = 'Europe/Warsaw',
+}
