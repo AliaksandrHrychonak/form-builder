6f2ec454e2a66eb842cde86beab16b744e30b99c
AliaksandrHrychonak <@icloud.com>
2025-04-13 23:09:59 +0300

    feat(signup): enhance signup page layout and UI
    Replaced placeholder div with a structured layout including `Logo` and `SignUp` components. Updated the page to use a responsive design with flexbox for better user experience.

--- frontend/src/pages/signup/ui/index.tsx
+++ frontend/src/pages/signup/ui/index.tsx
@@ -1,4 +1,6 @@
 import { generateLocalizedMetadata } from '@shared/lib';
+import { Logo } from '@shared/ui';
+import { SignUp } from '@widgets/signup/ui';
 
 import type { Metadata } from 'next';
 import type { FC, JSX } from 'react';
@@ -7,5 +9,12 @@ export const generateMetadataSignUpPage = async ({ params }: { params: { lang: s
     generateLocalizedMetadata(params, 'signup');
 
 export const SignUpPage: FC = (): JSX.Element => {
-    return <div>signup</div>;
+    return (
+        <main className='flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10'>
+            <section className='flex w-full max-w-md flex-col gap-6'>
+                <Logo />
+                <SignUp />
+            </section>
+        </main>
+    );
 };
