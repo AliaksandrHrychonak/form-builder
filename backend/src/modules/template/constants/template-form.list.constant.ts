08c041652051bb52f486db97b7538dad5b00a2b9
AliaksandrHrychonak <@icloud.com>
2025-04-22 23:16:43 +0300

    feat(template): enhance constants with new defaults and enums
    Added new constants for template forms and questions, including default order and available fields for ordering and searching. Extended status code enums for improved error handling in template-related operations.

---
+++ backend/src/modules/template/constants/template-form.list.constant.ts
@@ -0,0 +1,7 @@
+export const TEMPLATE_FORM_DEFAULT_USER_ORDER_BY = 'order';
+export const TEMPLATE_FORM_DEFAULT_USER_AVAILABLE_ORDER_BY = [
+    'createdAt',
+    'updatedAt',
+    'order',
+];
+export const TEMPLATE_FORM_DEFAULT_USER_AVAILABLE_SEARCH = [];
