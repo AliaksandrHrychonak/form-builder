66c80f15c8916ee5a83f948a02cc2f92ef35cf7b
AliaksandrHrychonak <@icloud.com>
2025-04-26 03:32:39 +0300

    feat(backend): remove TemplateProtected decorator
    Eliminated the TemplateProtected decorator as it is no longer required. This simplifies the codebase and removes unused functionality.

--- backend/src/modules/template/decorators/template.decorator.ts
+++
@@ -1,6 +0,0 @@
-import { UseGuards, applyDecorators } from '@nestjs/common';
-import { TemplateAccessGuard } from '../guards/template.guard';
-
-export function TemplateProtected(): MethodDecorator {
-    return applyDecorators(UseGuards(TemplateAccessGuard));
-}
