78a27772d31fa9bf6b1bf15a502d4ea9433442f4
AliaksandrHrychonak <@icloud.com>
2025-04-26 03:31:29 +0300

    feat(request): add Elasticsearch request interface support
    Introduced a new `IElasticsearchRequest` interface to handle pagination, search, and sorting for Elasticsearch-related requests. Updated `IRequestApp` to include the `__elasticsearch` property for enhanced Elastic-specific functionality.

--- backend/src/common/request/interfaces/request.interface.ts
+++ backend/src/common/request/interfaces/request.interface.ts
@@ -1,19 +1,41 @@
 import { Request } from 'express';
 import { ResponsePagingMetadataPaginationDto } from 'src/common/response/dtos/response.paging.dto';
+import { ENUM_ELASTICSEARCH_ORDER_DIRECTION_TYPE } from '../../elasticsearch/constants/elasticsearch.enum.constant';
+
+interface IElasticsearchRequest {
+    pagination?: {
+        page: number;
+        perPage: number;
+    };
+    search?: {
+        query: string;
+        availableFields: string[];
+    };
+    sort?: {
+        orderBy: string;
+        orderDirection: ENUM_ELASTICSEARCH_ORDER_DIRECTION_TYPE;
+        availableOrderBy: string[];
+        availableOrderDirection: ENUM_ELASTICSEARCH_ORDER_DIRECTION_TYPE[];
+    };
+}
 
 export interface IRequestApp<
     T = Record<string, any>,
     N = Record<string, any>,
     B = Record<string, any>,
     C = Record<string, any>,
+    F = Record<string, any>,
 > extends Request {
     apiKey?: B;
     user?: T;
     template?: C;
     __user?: N;
+    __template?: F;
 
     __language: string;
     __version: string;
 
     __pagination?: ResponsePagingMetadataPaginationDto;
+
+    __elasticsearch?: IElasticsearchRequest;
 }
