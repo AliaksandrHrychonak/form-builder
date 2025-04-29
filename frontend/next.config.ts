6409823ad56db30b61b82333871d73076a01eca2
AliaksandrHrychonak <@icloud.com>
2025-04-29 04:14:21 +0300

    feat(config): enhance Next.js config for image and path support
    Added `unoptimized` images configuration and dynamic `assetPrefix` and `basePath` handling. This improves flexibility for development and production environments.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
+    images: {
+        unoptimized: true,
+    },
+    assetPrefix: process.env.NODE_ENV === 'production' ? undefined : '',
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
 };
 
 export default nextConfig;
