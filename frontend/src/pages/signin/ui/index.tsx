026cc4aecdc8ba40dd929ef02171a8c42d3a4ef5
AliaksandrHrychonak <@icloud.com>
2025-04-29 04:27:40 +0300

    refactor: simplify Logo component usage
    Removed unnecessary className prop from the Logo component to streamline its usage and improve code readability. This change does not impact the functionality or appearance of the sign-in page.

--- frontend/src/pages/signin/ui/index.tsx
+++ frontend/src/pages/signin/ui/index.tsx
@@ -11,7 +11,7 @@ export const SignInPage = (): JSX.Element => {
     return (
         <main className='flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10'>
             <section className='flex w-full max-w-sm flex-col gap-6'>
-                <Logo className='' />
+                <Logo />
                 <SignIn />
             </section>
         </main>
