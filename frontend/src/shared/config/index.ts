7d3bfdf9ddb622f2a0a8a6a543e64b57088ce577
AliaksandrHrychonak <@icloud.com>
2025-04-29 04:22:29 +0300

    feat(config): add and structure app configuration constants
    Introduces multiple configuration constants including API details, app routes, and UI settings, sourced from environment variables. This improves the centralized management of application-specific settings and enhances maintainability.

--- frontend/src/shared/config/index.ts
+++ frontend/src/shared/config/index.ts
@@ -2,6 +2,24 @@ export * from './theme';
 export * from './i18n';
 export * from './meta';
 
-export const Config = {};
+export const Config = {
+    BASE_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    API_KEY_PUBLIC: process.env.NEXT_PUBLIC_API_KEY,
+    API_WITH_CREDENTIALS: process.env.NEXT_PUBLIC_API_WITH_CREDENTIALS === 'true',
+    API_KEY_PRIVATE: process.env.API_KEY_PRIVATE || '',
+    API_KEY_HEADER: process.env.NEXT_PUBLIC_API_KEY_HEADER,
+    API_LANG_HEADER: process.env.NEXT_PUBLIC_API_LANG_HEADER,
+    APP_ROUTES: {
+        SIGN_IN: 'auth/signin',
+        SIGN_UP: 'auth/signup',
+        SIGN_OUT_CALLBACK: '/',
+        TERMS_SERVICE: 'auth/terms',
+        PRIVACY_POLICY: 'auth/privacy',
+        FORGOT_PASSWORD: 'auth/forgot-password',
+    },
+    DEFAULT_DELAY: Number(process.env.NEXT_PUBLIC_DEFAULT_DELAY),
+    UI_COOKIE_MAX_AGE: Number(process.env.NEXT_PUBLIC_UI_COOKIE_MAX_AGE),
+    UI_MOBILE_BREAKPOINT: Number(process.env.NEXT_PUBLIC_UI_MOBILE_BREAKPOINT),
+} as const;
 
 export type Config = typeof Config;
