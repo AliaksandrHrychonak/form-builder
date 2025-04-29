2b897bbe6fa25bcfb1f57b112717fd764b36f9b5
AliaksandrHrychonak <@icloud.com>
2025-04-29 15:08:11 +0300

    fix(config): add fallback values for environment variables
    Ensure environment variables have default fallback values to prevent potential runtime errors. This change improves application stability and guarantees consistent behavior in the absence of specific environment configurations.

--- frontend/src/shared/config/index.ts
+++ frontend/src/shared/config/index.ts
@@ -1,10 +1,10 @@
 export const Config = {
-    BASE_API_URL: process.env.NEXT_PUBLIC_API_URL,
-    API_KEY_PUBLIC: process.env.NEXT_PUBLIC_API_KEY,
+    BASE_API_URL: process.env.NEXT_PUBLIC_API_URL ?? '',
+    API_KEY_PUBLIC: process.env.NEXT_PUBLIC_API_KEY ?? '',
     API_WITH_CREDENTIALS: process.env.NEXT_PUBLIC_API_WITH_CREDENTIALS === 'true',
-    API_KEY_PRIVATE: process.env.API_KEY_PRIVATE,
-    API_KEY_HEADER: process.env.NEXT_PUBLIC_API_KEY_HEADER,
-    API_LANG_HEADER: process.env.NEXT_PUBLIC_API_LANG_HEADER,
+    API_KEY_PRIVATE: process.env.API_KEY_PRIVATE ?? '',
+    API_KEY_HEADER: process.env.NEXT_PUBLIC_API_KEY_HEADER ?? '',
+    API_LANG_HEADER: process.env.NEXT_PUBLIC_API_LANG_HEADER ?? '',
     APP_ROUTES: {
         SIGN_IN: 'auth/signin',
         SIGN_UP: 'auth/signup',
@@ -13,9 +13,9 @@ export const Config = {
         PRIVACY_POLICY: 'auth/privacy',
         FORGOT_PASSWORD: 'auth/forgot-password',
     },
-    DEFAULT_DELAY: Number(process.env.NEXT_PUBLIC_DEFAULT_DELAY),
-    UI_COOKIE_MAX_AGE: Number(process.env.NEXT_PUBLIC_UI_COOKIE_MAX_AGE),
-    UI_MOBILE_BREAKPOINT: Number(process.env.NEXT_PUBLIC_UI_MOBILE_BREAKPOINT),
+    DEFAULT_DELAY: Number(process.env.NEXT_PUBLIC_DEFAULT_DELAY) ?? 500,
+    UI_COOKIE_MAX_AGE: Number(process.env.NEXT_PUBLIC_UI_COOKIE_MAX_AGE) ?? 604800,
+    UI_MOBILE_BREAKPOINT: Number(process.env.NEXT_PUBLIC_UI_MOBILE_BREAKPOINT) ?? 768,
 } as const;
 
 export type Config = typeof Config;
