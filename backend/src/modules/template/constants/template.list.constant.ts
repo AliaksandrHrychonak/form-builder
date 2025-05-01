8b23b28f4ffe651e8949f7cb2ca4928b6548c1c8
AliaksandrHrychonak <@icloud.com>
2025-05-01 14:29:49 +0300

    refactor(template): remove 'title' from order-by options
    Removed 'title' from the available order-by fields across public, user, and admin templates. This simplifies the order-by options, focusing on more relevant sorting criteria.

--- backend/src/modules/template/constants/template.list.constant.ts
+++ backend/src/modules/template/constants/template.list.constant.ts
@@ -1,7 +1,6 @@
 export const TEMPLATE_DEFAULT_PUBLIC_ORDER_BY = 'createdAt';
 export const TEMPLATE_DEFAULT_PUBLIC_AVAILABLE_ORDER_BY = [
     'createdAt',
-    'title',
     'popularityScore',
     'updatedAt',
     '_score',
@@ -26,7 +25,6 @@ export const TEMPLATE_DEFAULT_PUBLIC_AVAILABLE_SEARCH = [
 export const TEMPLATE_DEFAULT_USER_ORDER_BY = 'createdAt';
 export const TEMPLATE_DEFAULT_USER_AVAILABLE_ORDER_BY = [
     'createdAt',
-    'title',
     'popularityScore',
     'updatedAt',
     '_score',
@@ -52,7 +50,6 @@ export const TEMPLATE_DEFAULT_USER_AVAILABLE_SEARCH = [
 export const TEMPLATE_DEFAULT_ADMIN_ORDER_BY = 'createdAt';
 export const TEMPLATE_DEFAULT_ADMIN_AVAILABLE_ORDER_BY = [
     'createdAt',
-    'title',
     'popularityScore',
     'updatedAt',
     '_score',
