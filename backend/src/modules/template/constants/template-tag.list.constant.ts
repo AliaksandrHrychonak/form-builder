f4ea26c6f78267c574497089aab752d622f8e989
AliaksandrHrychonak <@icloud.com>
2025-05-01 04:25:42 +0300

    feat(template): add default constants for template tags
    Introduced constants for default ordering and searchable fields in admin and public template tags. These ensure consistency and reduce hardcoding in related functionalities.

---
+++ backend/src/modules/template/constants/template-tag.list.constant.ts
@@ -0,0 +1,9 @@
+export const TEMPLATE_TAG_ADMIN_DEFAULT_ORDER_BY = 'createdAt';
+export const TEMPLATE_TAG_ADMIN_DEFAULT_AVAILABLE_ORDER_BY = ['createdAt'];
+
+export const TEMPLATE_TAG_PUBLIC_DEFAULT_ORDER_BY = 'createdAt';
+export const TEMPLATE_TAG_PUBLIC_DEFAULT_AVAILABLE_ORDER_BY = ['createdAt'];
+export const TEMPLATE_TAG_PUBLIC_DEFAULT_AVAILABLE_SEARCH = [
+    'name',
+    'description',
+];
