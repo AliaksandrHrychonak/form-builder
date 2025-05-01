df29a2d74d46c2464d8635f66e8a727dbf7e66be
AliaksandrHrychonak <@icloud.com>
2025-05-01 04:28:35 +0300

    feat(app): add WithZodClient provider and refactor query config
    Refactored query client configuration to use constants from Config for better flexibility. Introduced a new WithZodClient provider to abstract Zod I18n setup. Removed redundant useZodI18n call from WithQueryClient.

--- frontend/src/app/providers/with-query-provider.tsx
+++ frontend/src/app/providers/with-query-provider.tsx
@@ -4,22 +4,20 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { type JSX, type ReactNode, useMemo } from 'react';
 
-import { useZodI18n } from '@shared/lib/i18n/use-zod-i18n';
+import { Config } from '@shared/config';
 
+const { QUERY_CLIENT } = Config;
 const configQueryClient = {
     defaultOptions: {
         queries: {
-            staleTime: 5 * 60 * 1000,
-            retry: 1,
+            staleTime: QUERY_CLIENT.STALE_TIME,
+            retry: QUERY_CLIENT.RETRY,
         },
     },
 };
 
 export const WithQueryClient = ({ children }: { children: ReactNode }): JSX.Element => {
     const queryClient = useMemo(() => new QueryClient(configQueryClient), []);
-
-    // TODO useZodI18n to provider
-    useZodI18n();
     return (
         <QueryClientProvider client={queryClient}>
             {children}
